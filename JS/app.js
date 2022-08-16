0334a36d616a2c39b2b6e567b036d35fdba13a17
Zohrab Semedzade <zohrabsemedzade3@gmail.com>
2022-08-16 13:22:28 +0400

    designing navbar

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,6 @@
 function changeBG(){
     let navbar = document.getElementById('nav-bar');
     let scrollValue = window.scrollY;
-    console.log(scrollValue);
     if(scrollValue < 100){
         navbar.classList.remove('bgColor');
     }else{
@@ -10,3 +9,14 @@ function changeBG(){
 };
 
 window.addEventListener('scroll', changeBG);
+
+function changeNavColor(){
+    let navLink = document.querySelectorAll('.nav-link');
+    for(let i = 0; i < navLink.length; i++){
+        function changeColor(){
+            navLink[i].classList.add('navColor');
+        }
+        navLink[i].addEventListener('click', changeColor);
+    }
+};
+changeNavColor();
